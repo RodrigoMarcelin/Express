72ae885c1c8d9ebbd42a3602edd80942478fde7a
RodrigoMarcelin <rodrigoamorimdhy@gmail.com>
2020-03-22 19:23:17 -0300

    adicionando put e delete

--- src/servidor.js
+++ src/servidor.js
@@ -3,6 +3,9 @@ const porta = 3003
 const express = require('express')
 const app = express()
 const bancoDeDados = require('./bancoDeDados')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req,res, next) => {
     res.send(bancoDeDados.getProdutos())//converte para JSON 
@@ -12,12 +15,26 @@ app.get('/produtos/:id', (req, res, next) => {
     res.send(bancoDeDados.getProduto(req.params.id))
 })
 
-app.post('/produtos/:id', (req, res, next) => {
+app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
         nome: req.body.name,
         preco: req.body.preco
     })
     res.send(produto)// JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)// JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto)
 })
 
 app.listen(porta, () => {
